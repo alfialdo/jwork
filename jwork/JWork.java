536bec443592eb294a24cc2e18401fef1f83e153
alfialdo <aldoalfi02@gmail.com>
2021-03-27 09:33:31 +0700

    Modul 3 - Post Test -  update Jwork.java

--- jwork/JWork.java
+++ jwork/JWork.java
@@ -10,7 +10,7 @@ public class JWork
         Recruiter recruiter1 = new Recruiter(1, "Muhammad Alfi A", "lalayeye@gmail.com", "08218832277", location1);
         Job job1 = new Job(2, 11200000, "AI" ,JobCategory.DataScientist, recruiter1);
         Jobseeker jobseeker1 = new Jobseeker(1, "Charlie", "yeyelala@gmail.com", "1234", "20-3-2021");
-        Invoice invoice1 = new Invoice(1, job1.getId(), 11200000, "27/3/2021", jobseeker1, PaymentType.EwalletPayment, InvoiceStatus.Finished);
+        Invoice invoice1 = new Invoice(1, job1.getId(), job1.getFee(), "27/3/2021", jobseeker1, PaymentType.EwalletPayment, InvoiceStatus.Finished);
 
 
         // Statement program        
